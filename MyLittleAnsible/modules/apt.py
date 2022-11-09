f0f4bf65675be98b120e6da55da68e2e345f6184
Ubuntu <ansible@dev.tiw0kzabscsuhbvsrg3dus1nyg.dx.internal.cloudapp.net>
2022-11-09 18:41:45 +0000

    Last Push

--- MyLittleAnsible/modules/apt.py
+++ MyLittleAnsible/modules/apt.py
@@ -12,13 +12,20 @@ class Apt(BaseModule.BaseModule) :
         self.mainLogger.info('host=' + ssh_client.get_transport().getpeername()[0] + ' op=' + self.name + ' ' + str(self.params))
         if self.params["state"] == "present" :
             stdin, stdout, stderr = ssh_client.exec_command(CONST.APT_UPDATE_COMMAND)
+            stderr = stderr.read().decode('ascii')
             result = stdout.channel.recv_exit_status()
             if result != 0 :
-                raise MyDeployerError()
+                raise MyDeployerError(stderr)
+            elif stderr.startsWith('E:') :
+                raise MyDeployerError(stderr)
+
             stdin, stdout, stderr = ssh_client.exec_command(CONST.APT_INSTALL_COMMAND + " " + self.params["name"])
+            stderr = stderr.read().decode('ascii')
             stdout.channel.recv_exit_status()
             if result != 0 :
-                raise MyDeployerError()
+                raise MyDeployerError(stderr)
+            elif stderr.startsWith('E:') :
+                raise MyDeployerError(stderr)
         elif self.params["state"] == "absent" :
             stdin, stdout, stderr = ssh_client.exec_command(CONST.APT_REMOVE_COMMAND + " " + self.params["name"])
             stdout.channel.recv_exit_status()
--- MyLittleAnsible/services/errors.py
+++ MyLittleAnsible/services/errors.py
@@ -5,3 +5,5 @@ Allow to catch internal runtime errors scoped to the My Deployer API.
 
 class MyDeployerError(Exception):
     """Internal Error used when running commands."""
+    def __init__(self, text):
+        self.errorText = text
